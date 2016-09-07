a4ac36df11f5e0ed92569df9b74535075c0da765
YunFeng <2249468906@qq.com>
2016-09-07 21:46:49 +0800

    第二次上传

--- SplashActivity.java
+++ SplashActivity.java
@@ -2,6 +2,7 @@ package com.aitecube.yunfeng.frametest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        TextView textView=new TextView(SplashActivity.this);
+        textView.setText("我是搞测试的！！！");
+        textView.setTextSize(30);
     }
 }
